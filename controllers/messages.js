e17462d91c01cd51b9c6150860ab220fd84c6af5
ZoomIsland <zacherymilan@gmail.com>
2020-10-01 13:43:20 -0600

    Nodemailer works with dummy text

--- controllers/messages.js
+++ controllers/messages.js
@@ -2,9 +2,9 @@ const db = require('../models/index')
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
-  // host: defaults to localhost, but needs to be the domain I get from heroku/netlify
-  port: 465,
-  secure: true,
+  host: 'smtp.mail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.username,
     pass: process.env.password
@@ -19,6 +19,15 @@ const index = (req, res) => {
 }
   
 const create = (req, res) => {
+  const message = {
+    from: "zachmilan@engineer.com",
+    to: "zacherymilan@gmail.com",
+    subject: "New message from your portfolio!",
+    text: "This is a test, only that."
+  }
+  transporter.sendMail(message, (err) => {
+    console.log(err)
+  })
   db.Message.create(req.body, (err, newMessage) => {
     if (err) console.log('Error at Message create:', err)
     res.status(200).json(newMessage)
